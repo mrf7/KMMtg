591d4d2806c20ccb9b62eddc6f2a39a1f7d8c730
Michael Friend <16885048+mrf7@users.noreply.github.com>
2025-07-31 16:26:09 -0400

    Also log update

--- utils/src/commonMain/kotlin/Log.kt
+++ utils/src/commonMain/kotlin/Log.kt
@@ -1,3 +1,6 @@
 import co.touchlab.kermit.Logger
+import co.touchlab.kermit.Severity
 
-inline fun <T> T.alsoLog(block: (T) -> String = { this.toString() }) = also { Logger.d(block(this)) }
+inline fun <T> T.alsoLog(level: (T) -> Severity = { Severity.Debug }, block: (T) -> String = { this.toString() }) = also { Logger.log(level(this), Logger.tag, null, block(this)) }
+
+inline fun <T> T.alsoLog(level: Severity, block: (T) -> String = { this.toString() }) = alsoLog({ level }, block)
